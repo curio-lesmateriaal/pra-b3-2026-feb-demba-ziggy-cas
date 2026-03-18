cb3f2e89009845694a53c07193c02e50cf399b53
dembakaspers <dembakaspers@gmail.com>
2026-03-18 10:26:43 +0100

    css aangepast

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <?php require_once 'head.php'; ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 
@@ -17,6 +17,10 @@
 
     </div>
     <a href="create.php">create</a>
+    <?php
+    
+
+     ?>
 </body>
 
 </html>
