f278625d003da0ceb9b2fcf26e4ba274da142691
dembakaspers <dembakaspers@gmail.com>
2026-03-18 09:51:47 +0100

    update

--- head.php
+++ head.php
@@ -2,5 +2,18 @@
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>../css/normalize.css">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+
+</div class ="nav">
+<nav>
+<a href="inlog.php">Home</a>
+<a href="statestiek.php">Statestieken</a>
+<a href="planbord.php">Planbord</a>
+</nav>
+</div>
+
+<h1>Planning app</h1>
+
+
+<!-- hier komt informatie over de website en waarvoor hij bedoelt is -->
--- index.php
+++ index.php
@@ -4,15 +4,19 @@
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
-    
+
     <div class="container">
 
 
     </div>
-
+    <a href="create.php">create</a>
 </body>
 
 </html>
