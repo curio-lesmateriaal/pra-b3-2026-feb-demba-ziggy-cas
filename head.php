a9eb41f6d3f6f74f341877822117d5df04529c37
Rudy <127566010+purpledelectric@users.noreply.github.com>
2026-03-18 10:08:50 +0100

    YES

--- head.php
+++ head.php
@@ -1,8 +1,8 @@
-<?php require_once 'backend/config.php'; ?>
+<?php require_once __DIR__ . '/backend/config.php'; ?>
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo $base_url; ?>../css/normalize.css">
+<link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
 
 </div class ="nav">
