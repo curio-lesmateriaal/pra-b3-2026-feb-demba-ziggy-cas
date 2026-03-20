17af73c9a56fb8f77cbfb9ca88432e98f798b5fc
dembakaspers <dembakaspers@gmail.com>
2026-03-20 12:30:13 +0100

    JA

--- create.php
+++ create.php
@@ -1,44 +1,49 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Takenlijst</title>
+    <title>Takenlijst</title>`
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
 
-<h2>Nieuwe taak toevoegen</h2>
-<form method="POST">
-    Titel: <input type="text" name="titel" required><br>
-    Beschrijving: <textarea name="beschrijving"></textarea><br>
-    <button type="submit">Toevoegen</button>
-</form>
+    <h2>Nieuwe taak toevoegen</h2>
+    <form method="POST">
+        Titel: <input type="text" name="titel" required><br>
+        Beschrijving: <textarea name="beschrijving"></textarea><br>
+        <button type="submit">Toevoegen</button>
+    </form>
 
-<h2>Overzicht taken</h2>
+    <h2>Overzicht taken</h2>
 
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Titel</th>
-        <th>Beschrijving</th>
-        <th>Status</th>
-        <th>Aangemaakt op</th>
-    </tr>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Titel</th>
+            <th>Beschrijving</th>
+            <th>Status</th>
+            <th>Aangemaakt op</th>
+        </tr>
 
-<?php
-$sql = "SELECT * FROM taken ORDER BY aangemaakt_op DESC";
-$result = $conn->query($sql);
+        <?php
+        $sql = "SELECT * FROM taken ORDER BY aangemaakt_op DESC";
+        $result = $conn->query($sql);
 
-while ($row = $result->fetch_assoc()) {
-    echo "<tr>
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
             <td>{$row['id']}</td>
             <td>{$row['titel']}</td>
             <td>{$row['beschrijving']}</td>
             <td>{$row['status']}</td>
             <td>{$row['aangemaakt_op']}</td>
           </tr>";
-}
-?>
+        }
+        ?>
 
-</table>
+    </table>
 
 </body>
+
 </html>
